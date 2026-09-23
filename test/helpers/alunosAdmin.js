3aecaabab4f601cdb3c0853a06e879f135b78d29
Denis <deniskarolak@gmail.com>
2026-09-23 15:33:49 -0300

    fix(test): remove prefixo Bearer duplicado no cadastro de aluno

--- test/helpers/alunosAdmin.js
+++ test/helpers/alunosAdmin.js
@@ -4,7 +4,7 @@ export async function cadastrarAluno(adminToken, dadosAluno) {
     const resposta = await api()
         .post('/api/admin/alunos')
         .set('Content-Type', 'application/json')
-        .set('Authorization', `Bearer ${adminToken}`)
+        .set('Authorization', `${adminToken}`)
         .send(dadosAluno);
  
     if (resposta.status !== 201) {
